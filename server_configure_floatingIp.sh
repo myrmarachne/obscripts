e7fd52f2d2fe967ffb2c4ac6ee8b5a3a73a72313
Karolina <cynk.karolina@gmail.com>
2018-12-20 13:46:51 +0100

    added some scripts

---
+++ server_configure_floatingIp.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+if [ $server_shared_int_network_floatingIp ]
+then
+    touch /root/iperf-server-ip
+    echo "$server_shared_int_network_floatingIp" > /root/iperf-server-ip
+    screen -d -m -S client iperf -c $server_shared_int_network_floatingIp -t 20
+else
+    touch /root/iperf-server-ip
+    echo "$server_private_floatingIp" > /root/iperf-server-ip
+    screen -d -m -S client iperf -c $server_private_floatingIp -t 20
+fi 
