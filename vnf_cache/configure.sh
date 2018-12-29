cfd83a68933b97da8659cb758d96cae10c8cb798
Karolina <cynk.karolina@gmail.com>
2018-12-29 14:34:32 +0100

    wip

--- vnf_cache/configure.sh
+++ vnf_cache/configure.sh
@@ -2,14 +2,21 @@
 
 if [ $server_shared_int_network_floatingIp ]
 then
-    touch /root/iperf-server-ip
-    echo "$server_shared_int_network_floatingIp" > /root/iperf-server-ip
+    touch /root/server-ip
+    echo "$server_shared_int_network_floatingIp" > /root/server-ip
     SERVER_IP_ADDR=$server_shared_int_network_floatingIp
 else
-    touch /root/iperf-server-ip
-    echo "$server_private_floatingIp" > /root/iperf-server-ip
+    touch /root/server-ip
+    echo "$server_private_floatingIp" > /root/server-ip
     SERVER_IP_ADDR=$server_private_floatingIp
 fi
+
+touch /root/cache-ip
+if [ $private_floatingIp ]
+then
+    echo $private_floatingIp > /root/cache-ip
+fi
+
 
 # TODO update with the floating IP
 export IP_ADDR=$(ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1')
