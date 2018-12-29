583c658e1606afb01ddeeafec7bfc7bd2b895335
Karolina <cynk.karolina@gmail.com>
2018-12-29 20:18:28 +0100

    wip

--- server_configure_floatingIp.sh
+++ server_configure_floatingIp.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
-touch /root/server-ip
 if [ $server_shared_int_network_floatingIp ]
 then
+    touch /root/server-ip
     echo "$server_shared_int_network_floatingIp" > /root/server-ip
+    #screen -d -m -S client iperf -c $server_shared_int_network_floatingIp -t 20
 else
+    touch /root/server-ip
     echo "$server_private_floatingIp" > /root/server-ip
+    #screen -d -m -S client iperf -c $server_private_floatingIp -t 20
 fi 
